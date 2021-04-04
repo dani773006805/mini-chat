b39894043afce3917dfafa6dad7999b5d1771602
Dani <dani>
2021-04-05 01:16:51 +0600

    forgot

---
+++ src/test/java/com/minichat/attractor/MiniChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minichat.attractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
